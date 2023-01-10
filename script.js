8dca8ada8258d5657b41463d31c4bca8f7eff269
Lisa <107352832+oddpharmacy@users.noreply.github.com>
2023-01-10 10:28:17 +0800

    Add player input check

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
+// Get rock, paper or scissors from com
 function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 let ranNumber = getComputerChoice(1, 3);
-let result;
+let computerSelection;
 
 switch (ranNumber) {
     case 1:
-        result = 'rock';
+        computerSelection = 'rock';
         break;
     
     case 2:
-        result = 'paper';
+        computerSelection = 'paper';
         break;
 
     case 3:
-        result = 'scissors';
+        computerSelection = 'scissors';
         break;
 }
 
-console.log(result);
+//
+
+// Get player input of rock, paper or scissors
+
+let playerSelection = prompt("Choose rock, paper or scissors");
+
+// Check player input 
+
+let checkPlayerSelection = playerSelection.toUpperCase();
+
+switch (true) {
+    case (checkPlayerSelection == 'ROCK'):
+    case (checkPlayerSelection == 'PAPER'):
+    case (checkPlayerSelection == 'SCISSORS'):
+        console.log("Player input recorded");
+        playerSelection = checkPlayerSelection;
+        checkPlayerSelection = true;
+        break;
+    
+    default:
+        console.log("Invalid input");
+        checkPlayerSelection = false;
+        break;
+}
+
